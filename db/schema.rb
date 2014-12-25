e0cad9efa8214ccb29edcede0c35ae0309f11f16
Vidul Parthasarathy <viparthasarathy@gmail.com>
2014-12-25 21:03:54 +0000

    Finish toy app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225195322) do
+ActiveRecord::Schema.define(version: 20141225203742) do
+
+  create_table "microposts", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
